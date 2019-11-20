0f5d47b06bc429caaf5604fb75e56ea4cacdb0a4
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 20:55:00 +0100

    Started schematic module

---
+++ src/main/java/de/lalaland/core/modules/schematics/SchematicModule.java
@@ -0,0 +1,31 @@
+package de.lalaland.core.modules.schematics;
+
+import de.lalaland.core.CorePlugin;
+import de.lalaland.core.modules.IModule;
+
+/*******************************************************
+ * Copyright (C) Gestankbratwurst suotokka@gmail.com
+ *
+ * This file is part of LaLaLand-CorePlugin and was created at the 20.11.2019
+ *
+ * LaLaLand-CorePlugin can not be copied and/or distributed without the express
+ * permission of the owner.
+ *
+ */
+public class SchematicModule implements IModule {
+
+  @Override
+  public String getModuleName() {
+    return "SchematicModule";
+  }
+
+  @Override
+  public void enable(final CorePlugin plugin) throws Exception, Exception {
+
+  }
+
+  @Override
+  public void disable(final CorePlugin plugin) throws Exception {
+
+  }
+}
---
+++ src/main/java/de/lalaland/core/modules/schematics/core/AbstractSchematic.java
@@ -0,0 +1,20 @@
+package de.lalaland.core.modules.schematics.core;
+
+/*******************************************************
+ * Copyright (C) Gestankbratwurst suotokka@gmail.com
+ *
+ * This file is part of LaLaLand-CorePlugin and was created at the 20.11.2019
+ *
+ * LaLaLand-CorePlugin can not be copied and/or distributed without the express
+ * permission of the owner.
+ *
+ */
+public abstract class AbstractSchematic {
+
+  public AbstractSchematic() {
+
+  }
+
+  public abstract void paste();
+
+}
