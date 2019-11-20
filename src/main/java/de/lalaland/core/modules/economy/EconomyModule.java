90c5c604eede1c1d30c26624992917c426c08242
Piinguiin <Carlosensale99>
2019-11-20 13:51:03 +0100

    Some work on EconomyModule

--- src/main/java/de/lalaland/core/modules/economy/EconomyModule.java
+++ src/main/java/de/lalaland/core/modules/economy/EconomyModule.java
@@ -2,27 +2,36 @@ package de.lalaland.core.modules.economy;
 
 import de.lalaland.core.CorePlugin;
 import de.lalaland.core.modules.IModule;
+import de.lalaland.core.modules.economy.gui.MobileEconomyGui;
+import org.bukkit.entity.Player;
 
 public class EconomyModule implements IModule {
 
-    private CorePlugin corePlugin;
+  private final CorePlugin corePlugin;
 
-    public EconomyModule(CorePlugin corePlugin){
-        this.corePlugin = corePlugin;
-    }
+  public EconomyModule(final CorePlugin corePlugin) {
+    this.corePlugin = corePlugin;
+  }
 
-    @Override
-    public void enable(CorePlugin plugin) throws Exception {
+  @Override
+  public void enable(final CorePlugin plugin) throws Exception {
 
-    }
+  }
 
-    @Override
-    public void disable(CorePlugin plugin) throws Exception {
+  @Override
+  public void disable(final CorePlugin plugin) throws Exception {
 
-    }
+  }
 
-    @Override
-    public String getModuleName() {
-        return "EconomyModule";
-    }
+  @Override
+  public String getModuleName() {
+    return "EconomyModule";
+  }
+
+  public void openMobileEconomyGui(final Player player) {
+
+    final MobileEconomyGui mobileEconomyGui = new MobileEconomyGui(player);
+    mobileEconomyGui.getMobileGui().open(player);
+  }
+
 }
--- src/main/java/de/lalaland/core/modules/economy/gui/MobileEconomyGui.java
+++ src/main/java/de/lalaland/core/modules/economy/gui/MobileEconomyGui.java
@@ -1,5 +1,22 @@
 package de.lalaland.core.modules.economy.gui;
 
+import de.lalaland.core.ui.gui.PrivateGui;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+
 public class MobileEconomyGui {
+
+  private Player player;
+  @Getter
+  private final PrivateGui mobileGui;
+
+  public MobileEconomyGui(final Player player) {
+    mobileGui = createMobileGui(player);
+  }
+
+  private PrivateGui createMobileGui(final Player player) {
+
+  }
+
 
 }
