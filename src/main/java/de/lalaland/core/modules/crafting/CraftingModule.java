fb728c888875e08e254c1a3ca7b17d75c009cb70
Piinguiin <Carlosensale99>
2019-11-23 16:58:33 +0100

    Worked on Crafting module

--- src/main/java/de/lalaland/core/modules/crafting/CraftingModule.java
+++ src/main/java/de/lalaland/core/modules/crafting/CraftingModule.java
@@ -1,5 +1,27 @@
 package de.lalaland.core.modules.crafting;
 
-public class CraftingModule {
+import de.lalaland.core.CorePlugin;
+import de.lalaland.core.modules.IModule;
+import de.lalaland.core.modules.crafting.listeners.CraftingInteractListener;
+import org.bukkit.Bukkit;
+
+public class CraftingModule implements IModule {
+
+  @Override
+  public String getModuleName() {
+    return "CraftingModule";
+  }
+
+  @Override
+  public void enable(final CorePlugin plugin) throws Exception {
+
+    Bukkit.getPluginManager().registerEvents(new CraftingInteractListener(plugin),plugin);
+  }
+
+  @Override
+  public void disable(final CorePlugin plugin) throws Exception {
+
+  }
+
 
 }
